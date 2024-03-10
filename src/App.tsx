639f84d0abe86ef549dc0d93218f2c2e80b38719
And4ik <And4ik11@gmail.com>
2024-03-10 14:11:02 +0100

    next logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,56 @@ import './App.css';
 import {Button} from "./Components/Button";
 
 function App() {
-    const [count, setCount] = useState(0)
-    const [maxValue, setMaxValue] = useState<number>(0)
+    const [count, setCount] = useState<number | string>(0)
+    const [maxValue, setMaxValue] = useState<number>(5)
     const [startValue, setStartValue] = useState<number>(0)
     const [error, setError] = useState("")
-
-    
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+    const [incResetButtonDisabled, setIncResetButtonDisabled] = useState(false)
+    const [valueForUser, setValueForUser] = useState<string>("")
     const incrementCount = () => {
-        setCount(count+1)
+        setCount(Number(count)+1)
     }
     const resetCount = () => {
       setCount(startValue)
     }
-
-
     const onChangeMaxHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setMaxValue(Number(e.currentTarget.value))
-    }
-    const onChangeStartHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        if (Number(e.currentTarget.value) < 0){
-            setError("не валидное число")
+        if (Number(e.currentTarget.value) <= startValue || startValue<0){
+            setError("Incorrect value!")
         }else {
             setError("")
         }
+        setMaxValue(Number(e.currentTarget.value))
+        setIsButtonDisabled(false)
+        setIncResetButtonDisabled(true)
+        setCount("press `set`")
+    }
+    const onChangeStartHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        if (Number(e.currentTarget.value) < 0 || Number(e.currentTarget.value) >= maxValue){
+            setError("Incorrect value!")
+        }else {
+            setError("")
+        }
+        // setValueForUser("enter values and press `set`")
         setStartValue(Number(e.currentTarget.value))
+        setIsButtonDisabled(false)
+        setIncResetButtonDisabled(true)
+        setCount("press `set`")
     }
 
     const onSetHandler = () => {
-      setCount(startValue)
+         setCount(startValue)
+        setIsButtonDisabled(true)
+        setIncResetButtonDisabled(false)
 
     }
+
   return (
       <div className="App">
           <div>
               <span>max value</span>
               <input
+                  className={startValue>=maxValue ? "ErrorColorInput":""}
                   type="number"
                   value={maxValue}
                   onChange={onChangeMaxHandler}
@@ -45,17 +60,20 @@ function App() {
           </div>
           <div>
               <span>start value</span>
-              <input type="number"
+              <input
+                  className={startValue< 0 || startValue>=maxValue ? "ErrorColorInput":"" }
+                    type="number"
                      value={startValue}
                      onChange={onChangeStartHandler}
               />
-              <div><Button disabled={Boolean(error)} name="set" onClick={onSetHandler}/></div>
+              <div><Button disabled={Boolean(error)|| startValue>=maxValue || isButtonDisabled } name="set" onClick={onSetHandler}/></div>
           </div>
           <div>
-              <div className={count >= maxValue  ? "maxCount" : ""}>{startValue < 0  ? error : count}</div>
-              <Button disabled={count >= maxValue} name={"inc"} onClick={incrementCount}/>
-              <Button disabled={false} name={"reset"} onClick={resetCount}/>
+              <div className={count >= maxValue  ? "maxCount" : ""}>{Boolean(error) ? error : count}</div>
+              <Button disabled={count >= maxValue ||incResetButtonDisabled } name={"inc"} onClick={incrementCount}/>
+              <Button disabled={startValue<0 || incResetButtonDisabled} name={"reset"} onClick={resetCount}/>
           </div>
+
 
       </div>
   );
